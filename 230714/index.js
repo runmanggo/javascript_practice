c5e052b51e1c68b00e9004b1ca199c7885c5084d
runmanggo <runmanggo@gmail.com>
2023-07-22 22:36:08 +0900

    js 배운걸로 만든 원기옥 페이지

---
+++ 230714/index.js
@@ -0,0 +1,18 @@
+// document.getElementsByClassName("");
+
+// const age = 10;
+
+// if (age > 19) {
+//   console.log("환영합니다");
+// } else if (age === 19) {
+//   console.log("수능잘치세요");
+// } else {
+//   console.log("안녕히 가세요");
+// }
+
+console.log(apple);
+
+// apple = 26;
+// let apple;
+
+// console.log(apple);
