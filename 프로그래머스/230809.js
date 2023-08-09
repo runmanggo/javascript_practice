a3cb38e0b51e3316b06eb121327d5953463a5fa5
runmanggo <runmanggo@gmail.com>
2023-08-09 21:26:15 +0900

    repalce / repalceAll 차이

--- 프로그래머스/230809.js
+++ 프로그래머스/230809.js
@@ -26,3 +26,21 @@ function solution(my_string) {
 /**Spread  > 객체 혹은 배열을 펼칠 수 있다
  * > 기존의 것은 건들지 않고 새로운 객체를 만들때 사용
  */
+
+//특정문자 제거하기
+function solution(my_string, letter) {
+  var answer = my_string.replaceAll(letter, "");
+  return answer;
+}
+
+/** 처음에는 replace를 사용함 하지만 두번째 답안때 해당문자 단어에서 한개만 없어졌음,
+ * replaceAll 메서드를 알게됨 > letter : 제거하는 문자, my_string에 제거하는 문자에 해당되는 모든 문자를 제거해줌
+ */
+
+// 다른 사람 풀이
+
+function solution(my_string, letter) {
+  const answer = my_string.split(letter).join("");
+  return answer;
+}
+/**split 인자를 기준으로 분리 > 배열로 변환 */
