60156f5bdc975d49adec71f6f0ea6a599a2006fb
망꼬 <130226437+runmanggo@users.noreply.github.com>
2023-10-04 23:34:42 +0900

    10/4
    한문제 더 풀고 커밋

--- 프로그래머스/231004.js
+++ 프로그래머스/231004.js
@@ -17,8 +17,16 @@ function solution(number, n, m) {
 }
 
 // 홀짝에 따라 다른 값 반환하기
-
 function solution(n) {
-  var answer = 0;
+  let answer = 0;
+  if (n % 2 === 0) {
+    for (let i = 2; i <= n; i += 2) {
+      answer += Math.pow(i, 2);
+    }
+  } else {
+    for (let i = 1; i <= n; i += 2) {
+      answer += i;
+    }
+  }
   return answer;
 }
