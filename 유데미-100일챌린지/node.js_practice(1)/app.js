484bc255688bf06a9df53af66b2d3b8c8eefbf8a
망꼬 <130226437+runmanggo@users.noreply.github.com>
2023-10-07 21:52:11 +0900

    10/7
    - node.js 동적 경로
    - 고유 id  설정
    - 세부 데이터 로드 설정
    - 404 /500 페이지 표시

--- 유데미-100일챌린지/node.js_practice(1)/app.js
+++ 유데미-100일챌린지/node.js_practice(1)/app.js
@@ -2,13 +2,14 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const uuid = require("uuid");
 const app = express();
 
 //ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static("public")); //Express 앱에 정적 파일 서비스를 추가
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
@@ -31,6 +32,32 @@ app.get("/restaurants", (req, res) => {
   });
 });
 
+//동적 라우팅 만들기
+//사용자의 구체적인 값을 얻기 위해. req쓰고, 엑세스 할 수 있도록 params키를 작성
+/**
+ * prams는 실제 객체가 포함되어 있고 해당 객체에는 이경로의 속성으로 id 로 정의한 동적 플레이스 홀더가 있음
+ */
+app.get("/restaurants/:id", (req, res) => {
+  const restaurantID = req.params.id;
+
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantID) {
+      return res.render("restaurant-detail", { restaurant: restaurant });
+    }
+  }
+
+  res.render("404");
+});
+/**
+ * restaurant: restaurant  > 왼쪽은 restaurant-detail 에서 사용한 키
+ * 오른쪽은 for문의 const restaurant 가리키고 있음
+ *
+ */
+
 app.get("/recommend", (req, res) => {
   // const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   // res.sendFile(htmlFilePath);
@@ -39,6 +66,7 @@ app.get("/recommend", (req, res) => {
 
 app.post("/recommend", (req, res) => {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
 
   const filePath = path.join(__dirname, "data", "restaurants.json");
   const fileData = fs.readFileSync(filePath);
@@ -61,6 +89,14 @@ app.get("/about", (req, res) => {
   // const htmlFilePath = path.join(__dirname, "views", "about.html");
   // res.sendFile(htmlFilePath);
   res.render("about");
+});
+
+app.use((req, res) => {
+  res.render("404");
+});
+
+app.use((error, req, res, next) => {
+  res.render("500");
 });
 
 app.listen(3000, () => {
