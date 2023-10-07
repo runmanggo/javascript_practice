35376b7c7fd18935a8626af040e4e24d71407e33
망꼬 <130226437+runmanggo@users.noreply.github.com>
2023-10-08 00:56:21 +0900

    10/8
    - 서치바
    -api 연동

--- HarryPotter_searchBar/app.js
+++ HarryPotter_searchBar/app.js
@@ -1,17 +1,35 @@
 const charactersList = document.getElementById("charactersList");
+const searchBar = document.getElementById("searchBar");
+let hpCharacters = [];
+
+searchBar.addEventListener("keyup", (event) => {
+  const searchString = event.target.value.toLowerCase();
+  const filteredCharacters = hpCharacters.filter((character) => {
+    return (
+      character.name.toLowerCase().includes(searchString) ||
+      character.house.toLowerCase().includes(searchString)
+    );
+  });
+  displayCharaters(filteredCharacters);
+});
 
 const harryApi = fetch("https://hp-api.onrender.com/api/characters")
   .then((response) => response.json())
-  .then((data) => displayCharaters(data));
+  .then((data) => {
+    hpCharacters = data;
+    displayCharaters(hpCharacters);
+  });
 
 const displayCharaters = (characters) => {
   const htmlString = characters
-    .map((character) => {
-      return `<li class="character"> <h2>${character.name}</h2>
-    <p>House: ${character.house}</p>
-    <img src="${character.image}"></img>
-</li>`;
-    })
+    .map(
+      (character) =>
+        `<li class="character"> 
+        <h2>${character.name}</h2>
+        <p>House: ${character.house}</p>
+        <img src="${character.image}"></img>
+        </li>`
+    )
     .join("");
   charactersList.innerHTML = htmlString;
 };
