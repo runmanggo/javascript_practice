1e03c992bc88b3655d7353484245aa970f1bf2d0
망꼬 <130226437+runmanggo@users.noreply.github.com>
2023-10-12 19:22:36 +0900

    10/12
    -내가 해냄!

--- 프로그래머스/231012.js
+++ 프로그래머스/231012.js
@@ -4,3 +4,20 @@ function solution(n_str) {
   var answer = +n_str;
   return answer;
 }
+
+//조건에 맞게 수열 변환하기 3
+
+function solution(arr, k) {
+  let answer = [];
+
+  if (k % 2 === 0) {
+    for (let i = 0; i < arr.length; i++) {
+      answer.push(arr[i] + k);
+    }
+  } else {
+    for (let j = 0; j < arr.length; j++) {
+      answer.push(arr[j] * k);
+    }
+  }
+  return answer;
+}
